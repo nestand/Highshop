4a2653b0dc9de2f48609e486ac1fca282854ca78
Andrey NESTEROV <andrey.nesterov@laposte.net>
2021-05-28 11:19:24 +0200

    the model Product is created, Test to show and get the results from DB -> OK

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -5,11 +5,24 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Product;
 
 class HomeController extends Controller
 {
     //method to create the view of the index page
+
+    //test connection to DB to get and show the results on the screen
     public function index (){
-        return view('home.index');
+    $products = Product::all();
+    foreach ($products as $product){
+        echo 'Title: '.$product->title;
+        echo "<br>";
+        echo 'Price: '.$product->price;
+        echo "<br>";
+        echo 'Description: '.$product->description;
+        echo "<br>";
+    } 
+     
+       // return view('home.index');
     }
 }
