7fbcc2101792971a82211bbefff5b26fc1c27d0f
Andrey NESTEROV <andrey.nesterov@laposte.net>
2021-06-04 12:27:39 +0200

    small fix of routes to show the product correctly in url

--- resources/views/categories/index.blade.php
+++ resources/views/categories/index.blade.php
@@ -66,19 +66,22 @@
                         {{-- function products from category.php--}}
 						@foreach ($cat->products as $product)
 						<!-- Product -->
-                        @php
-                            $image = '';
-                            if(count($product->images) > 0){
-                                $image = $product->images[0]['img'];
-                            } else {
-                                $image = 'no_image.png';
-                            }
-                        @endphp
+						@php
+						$image='';
+						if(count ($product->images) > 0){
+							$image = $product->images[0]['img'];
+						} else {
+							// if no photo available, it will be remplaced by the 'comming soon pic'
+							$image = 'soon.png';
+						}
+						
+						@endphp
+						
                         <div class="product">
                             <div class="product_image"><img src="/images/{{$image}}" alt="{{$product->title}}"></div>
-                            <div class="product_extra product_new"><a href="{{route('getCategories', ['category', $product->category['alias']] )}}">{{$product->category['title']}}</a></div>
+                            <div class="product_extra product_new"><a href="{{route('getCategories', $product->category['alias'] )}}">{{$product->category['title']}}</a></div>
                             <div class="product_content">
-                                <div class="product_title"><a href="{{ route('getProduct', ['category', $product->id]) }}">{{$product->title}}</a></div>
+                                <div class="product_title"><a href="{{ route('getProduct', [$product->category['alias'],$product->id]) }}">{{$product->title}}</a></div>
                                 @if ($product->new_price != null)
                                     <div style="text-decoration: line-through">${{$product->price}}</div>
                                     <div class="product_price">${{$product->new_price}}</div>
--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -34,13 +34,13 @@
 							<nav class="main_nav">
 								<ul>
 									<li class="hassubs active">
-										<a href="index.html">Home</a>
+										<a href="/">Home</a>
 										<ul>
-											<li><a href="categories.html">Categories</a></li>
-											<li><a href="product.html">Product</a></li>
-											<li><a href="cart.html">Cart</a></li>
-											<li><a href="checkout.html">Check out</a></li>
-											<li><a href="contact.html">Contact</a></li>
+											<li><a href="#">Categories</a></li>
+											<li><a href="#">Product</a></li>
+											<li><a href="#">Cart</a></li>
+											<li><a href="#">Check out</a></li>
+											<li><a href="#">Contact</a></li>
 										</ul>
 									</li>
 									<li class="hassubs">
